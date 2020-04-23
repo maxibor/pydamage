dcdcbc13e490aaf21344465cacbaaf147df05c7a
maxibor <maxime.borry@gmail.com>
2020-04-23 16:09:56 +0200

    change loss function to 'huber'

--- pydamage/optim.py
+++ pydamage/optim.py
@@ -4,7 +4,7 @@
 from scipy.optimize import curve_fit
 
 
-def optim(function, parameters, xdata, ydata, bounds):
+def optim(function, parameters, xdata, ydata, bounds, loss='huber'):
     """Find optimal parameters given data
 
     Args:
@@ -13,9 +13,10 @@ def optim(function, parameters, xdata, ydata, bounds):
         ydata (np array): y values
         bounds (tuple of tuple): optimization bounds
                     ((par1_min, par2_min), (par1_max, par2_max))
+        loss (str): loss function. See https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html#scipy.optimize.least_squares
     Returns:
         (dict): 'parameter_name':'parameter_value'
     """
-    op_par = curve_fit(function, xdata=xdata, ydata=ydata, bounds=bounds)
+    op_par = curve_fit(function, xdata=xdata, ydata=ydata, bounds=bounds, loss=loss)
     op_par_dict = {k: v for (k, v) in zip(parameters, op_par[0])}
     return(op_par_dict)
