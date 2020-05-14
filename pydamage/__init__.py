ddf32add4fb718d525552f5c0c08648c5acd94e3
maxibor <maxime.borry@gmail.com>
2020-05-14 14:53:49 +0200

    properly single sourcing version

--- pydamage/__init__.py
+++ pydamage/__init__.py
@@ -1,2 +1,2 @@
+__version__ = '0.1'
 from pydamage.main import analyze
-from pydamage.version import __version__
--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from pydamage.version import __version__
 from setuptools import setup, find_packages
+import codecs
+import os.path
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 setup(
     name='pydamage',
-    version=__version__,
+    version=get_version("pydamage/__init__.py"),
     description='Damage parameter estimation for ancient DNA',
     long_description=open("README.md").read(),
     url='https://github.com/maxibor/pydamage',
