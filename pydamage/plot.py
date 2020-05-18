1b1ff67e2b2c727b5c0126c5033cc3c55aa513a2
maxibor <maxime.borry@gmail.com>
2020-05-19 01:22:27 +0200

    add residual plots

--- pydamage/plot.py
+++ pydamage/plot.py
@@ -3,6 +3,7 @@ from pydamage.models import geom_mod, unif_mod
 from pydamage import utils
 import numpy as np
 from os import makedirs
+from scipy.stats import probplot
 
 
 def damageplot(damage_dict, wlen, outdir):
@@ -29,6 +30,7 @@ def damageplot(damage_dict, wlen, outdir):
     contig = damage_dict['reference']
     pvalue = damage_dict['pvalue']
     coverage = damage_dict['coverage']
+    residuals = damage_dict['residuals']
     plotdir = outdir
 
     if pvalue < 0.001:
@@ -57,48 +59,60 @@ def damageplot(damage_dict, wlen, outdir):
     y_geom_high = np.minimum(np.ones(y_geom.shape[0]), geom.pmf(
         x, geom_p, geom_pmin_high, geom_pmax_high))
 
-    fig = plt.figure(figsize=(12, 8), dpi=100, facecolor='w', edgecolor='k')
-    ax = fig.add_subplot(111)
-    ax.xaxis.labelpad = 20
-    ax.yaxis.labelpad = 20
-
-    plt.plot(qlen, c2t,
-             color='#bd0d45',
-             alpha=0.1,
-             label='C to T transitions')
-
-    plt.plot(qlen, g2a,
-             color='#236cf5',
-             alpha=0.1,
-             label='G to A transitions')
-
-    plt.plot(x, y_unif,
-             linewidth=2.5,
-             color='DarkOliveGreen',
-             alpha=0.8,
-             label='Uniform model')
-
-    plt.fill_between(x, y_unif_low, y_unif_high,
-                     color='DarkOliveGreen',
-                     alpha=0.1,
-                     label='Uniform CI (2 sigma)')
-
-    plt.plot(x, y_geom,
-             linewidth=2.5,
-             color='#D7880F',
-             alpha=0.8,
-             label='Geometric model')
-
-    plt.fill_between(x, y_geom_low, y_geom_high,
-                     color='#D7880F',
-                     alpha=0.1,
-                     label='Geometric CI (2 sigma)')
-
-    plt.xlabel("Base from 5'", fontsize=20)
-    plt.ylabel("Substitution frequency", fontsize=20)
-    plt.suptitle(f"{contig}", fontsize=20, y=0.95)
-    plt.title(f"coverage: {round(coverage,2)} | pvalue{rpval}", fontsize=12)
     plt.xticks(rotation=45, fontsize=8)
-    ax.legend(fontsize=12)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(qlen, c2t,
+            color='#bd0d45',
+            alpha=0.1,
+            label='C to T transitions')
+
+    ax.plot(qlen, g2a,
+            color='#236cf5',
+            alpha=0.1,
+            label='G to A transitions')
+
+    ax.plot(x, y_unif,
+            linewidth=2.5,
+            color='DarkOliveGreen',
+            alpha=0.8,
+            label='Uniform model')
+
+    ax.fill_between(x, y_unif_low, y_unif_high,
+                    color='DarkOliveGreen',
+                    alpha=0.1,
+                    label='Uniform CI (2 sigma)')
+
+    ax.plot(x, y_geom,
+            linewidth=2.5,
+            color='#D7880F',
+            alpha=0.8,
+            label='Geometric model')
+
+    ax.fill_between(x, y_geom_low, y_geom_high,
+                    color='#D7880F',
+                    alpha=0.1,
+                    label='Geometric CI (2 sigma)')
+
+    ax.set_xlabel("Base from 5'", fontsize=10)
+    ax.set_ylabel("Substitution frequency", fontsize=10)
     ax.set_xticks(qlen)
+    ax.set_xticklabels(qlen, rotation=45, fontsize=6)
+    ax.set_title(f"coverage: {round(coverage,2)} | pvalue{rpval}", fontsize=8)
+    ax.legend(fontsize=8)
+    # ax.set_title(f"coverage: {round(coverage,2)} | pvalue{rpval}", fontsize=8)
+
+    left, bottom, width, height = [0.65, 0.3, 0.2, 0.2]
+    ax2 = fig.add_axes([left, bottom, width, height])
+    # ax2.hist(residuals, bins='auto')
+    probplot(residuals, plot=ax2, sparams=(0, np.std(c2t)))
+    ax2.set_xlabel("Observed value", fontsize=6)
+    ax2.set_ylabel("Theoretical quantile", fontsize=6)
+    ax2.set_title("QQplot of residuals", fontsize=8)
+    ax2.set_xticklabels([round(i, 3) for i in ax2.get_xticks()], fontsize=6)
+    ax2.set_yticklabels([round(i, 3) for i in ax2.get_yticks()], fontsize=6)
+
+    plt.suptitle(contig, fontsize=12, y=0.95)
+
     plt.savefig(f"{plotdir}/{contig}.png", dpi=200)
